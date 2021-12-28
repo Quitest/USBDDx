20ea798f7db715e2cf26a0cf5e08d6990a806531
lenovo <none@none.org>
2021-12-28 15:25:10 +0300

    USBDDx-27 Сбор информации о системе
    Init. Написан мусор...

---
+++ usbddc/src/main/java/ru/pel/usbddc/service/SystemInfoCollector.java
@@ -0,0 +1,25 @@
+package ru.pel.usbddc.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.pel.usbddc.entity.USBDevice;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class SystemInfoCollector {
+    private Map<String, String> systemEnvironment;
+    private Map<String, USBDevice> usbDeviceMap;
+
+    public void collectSystemInfo() throws IOException {
+        systemEnvironment = System.getenv();
+        usbDeviceMap = new RegistryAnalyzer().getRegistryAnalysis(true);
+        List<Path> logList = new OSInfoCollector().getSetupapiDevLogList();
+        usbDeviceMap = new SetupapiDevLogAnalyzer(usbDeviceMap, logList)
+                .getAnalysis(true);
+    }
+}
---
+++ usbddc/src/test/java/ru/pel/usbddc/service/SystemInfoCollectorTest.java
@@ -0,0 +1,13 @@
+package ru.pel.usbddc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SystemInfoCollectorTest {
+
+    @Test
+    void garbage(){
+        SystemInfoCollector systemInfoCollector = new SystemInfoCollector();
+    }
+}
