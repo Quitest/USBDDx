446809298bcd004b1e7289677bddf800612ca747
lenovo <none@none.org>
2022-02-07 13:40:38 +0300

    USBDDx-20 Настройка Usb.ids
    Указание на используемый файл usb.ids вынесено в конфиг файл. Конфиг файл читается UsbddcConfig, а уже из него берутся конфигурационные параметры во всем приложении.

--- usbddc/src/main/java/ru/pel/usbddc/config/UsbddcConfig.java
+++ usbddc/src/main/java/ru/pel/usbddc/config/UsbddcConfig.java
@@ -1,26 +1,79 @@
 package ru.pel.usbddc.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
-public class UsbddcConfig extends UsbddcDefaultConfig{
+/**
+ * Конфигурационный класс.
+ * <p>
+ * Данные берет из файла, указанного при инициализации или файла по умолчанию.
+ * Если файл недоступен или по каким-либо причинам не удалось прочитать данные, то используется конфигурация по умолчанию.
+ */
+@Getter
+@Setter
+public class UsbddcConfig {
     private static final Logger logger = LoggerFactory.getLogger(UsbddcConfig.class);
+    //TODO а нужен ли тут вообще синглтон, может все поля перегнать в статик и класс сделать utility?
+    private static volatile UsbddcConfig instance;
+    private int threadPoolSize;
+    private String usbIdsPath;
 
-    public UsbddcConfig(String pathToConfig){
+    {
+        threadPoolSize = 8;
+        usbIdsPath = "usb.ids";
+    }
+
+    private UsbddcConfig(String pathToConfig) {
 
         try {
             Properties config = new Properties();
-            config.loadFromXML(new FileInputStream(pathToConfig));
+            FileInputStream file = new FileInputStream(pathToConfig);
+            config.loadFromXML(file);
             threadPoolSize = Integer.parseInt(config.getProperty("threadPoolSize"));
             usbIdsPath = config.getProperty("usbIdsPath");
 
         } catch (IOException e) {
-            logger.warn("Не удалось загрузить конфигурацию из файла {}. Используется конфигурация по умолчанию. Причина: {}",
+            logger.warn("Используется конфигурация по умолчанию, т.к. не удалось загрузить конфигурацию из файла {}. Причина: {}",
                     pathToConfig, e.getLocalizedMessage());
         }
+    }
+
+    /**
+     * Получить конфигурацию из указанного файла.
+     *
+     * @param pathToConfig XML-файл конфигурации.
+     * @return инстанс конфига
+     */
+    public static UsbddcConfig getInstance(String pathToConfig) {
+        UsbddcConfig result = instance;
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (UsbddcConfig.class) {
+            if (instance == null) {
+                instance = new UsbddcConfig(pathToConfig);
+            }
+
+            return instance;
+        }
+    }
+
+    /**
+     * Получить конфигурацию из файла по умолчанию - config.xml
+     *
+     * @return инстанс конфига
+     */
+    public static UsbddcConfig getInstance() {
+        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(""))
+                .getPath();
+        return getInstance(rootPath + "config.xml");
     }
 }
