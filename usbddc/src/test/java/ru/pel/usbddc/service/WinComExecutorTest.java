a62d75b484d09d7b9a267b35a1dafd94c5b2f89b
lenovo <none@none.org>
2022-02-25 15:25:46 +0300

    USBDDx-55 WinConExecutor должен возвращать сообщения
    savepoint

--- usbddc/src/test/java/ru/pel/usbddc/service/WinComExecutorTest.java
+++ usbddc/src/test/java/ru/pel/usbddc/service/WinComExecutorTest.java
@@ -3,7 +3,11 @@ package ru.pel.usbddc.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 
@@ -12,22 +16,22 @@ import static org.hamcrest.Matchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class WinComExecutorTest {
-
-    private static final WinComExecutor WIN_COM_EXECUTOR = new WinComExecutor();
-
     private final String NODE_NAME = "HKEY_LOCAL_MACHINE\\tempHive";
     private final String HIVE = "C:\\Users\\Default\\ntuser.dat";
     private final String LOAD_HIVE_COMMAND = String.format("reg load %s %s", NODE_NAME, HIVE);
     private final String UNLOAD_HIVE = String.format("reg unload %s", NODE_NAME);
+    @Mock
+    private Runtime runtimeMock;
+    private final WinComExecutor winComExecutor = new WinComExecutor(runtimeMock);
 
     @Test
     @DisplayName("Команда выполнена с ошибкой - body содержит описание ошибки")
     void whenExecCommandFailBodyContainsDescription() throws IOException, InterruptedException {
         try {
-            WinComExecutor.Result<Integer, String> result = WIN_COM_EXECUTOR.exec(LOAD_HIVE_COMMAND);
+            WinComExecutor.Result<Integer, String> result = winComExecutor.exec(LOAD_HIVE_COMMAND);
             assertThat(result.getBody(), not(emptyOrNullString()));
         } finally {
-            WIN_COM_EXECUTOR.exec(UNLOAD_HIVE);
+            winComExecutor.exec(UNLOAD_HIVE);
         }
     }
 
@@ -35,10 +39,10 @@ class WinComExecutorTest {
     @DisplayName("Команда выполнена с ошибкой - exit code НЕ равен нулю")
     void whenExecCommandFailExitCodeNotEqualToZero() throws IOException, InterruptedException {
         try {
-            WinComExecutor.Result<Integer, String> result = WIN_COM_EXECUTOR.exec(LOAD_HIVE_COMMAND);
+            WinComExecutor.Result<Integer, String> result = winComExecutor.exec(LOAD_HIVE_COMMAND);
             assertThat(result.getExitCode(), not(equalTo(0)));
         } finally {
-            WIN_COM_EXECUTOR.exec(UNLOAD_HIVE);
+            winComExecutor.exec(UNLOAD_HIVE);
         }
     }
 
@@ -46,10 +50,10 @@ class WinComExecutorTest {
     @DisplayName("Команда выполнена успешно - body содержит текстовый результат")
     void whenExecCommandSuccessfulBodyContainsStringResult() throws IOException, InterruptedException {
         try {
-            WinComExecutor.Result<Integer, String> result = WIN_COM_EXECUTOR.exec(LOAD_HIVE_COMMAND);
+            WinComExecutor.Result<Integer, String> result = winComExecutor.exec(LOAD_HIVE_COMMAND);
             assertThat(result.getBody(), not(blankOrNullString()));
         } finally {
-            WIN_COM_EXECUTOR.exec(UNLOAD_HIVE);
+            winComExecutor.exec(UNLOAD_HIVE);
         }
     }
 
@@ -57,10 +61,10 @@ class WinComExecutorTest {
     @DisplayName("Команда выполнена успешно - exit code РАВЕН 0")
     void whenExecCommandSuccessfulExitCodeEqualToZero() throws IOException, InterruptedException {
         try {
-            WinComExecutor.Result<Integer, String> result = WIN_COM_EXECUTOR.exec(LOAD_HIVE_COMMAND);
+            WinComExecutor.Result<Integer, String> result = winComExecutor.exec(LOAD_HIVE_COMMAND);
             assertThat(result.getExitCode(), equalTo(0));
         } finally {
-            WIN_COM_EXECUTOR.exec(UNLOAD_HIVE);
+            winComExecutor.exec(UNLOAD_HIVE);
         }
     }
 }
