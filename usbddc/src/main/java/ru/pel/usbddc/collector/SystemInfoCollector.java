ba3b2d5eb779af69dec8fda9a435a8182cef5354
lenovo <none@none.org>
2022-03-05 15:25:42 +0300

    USBDDx-52 Сведения о правах в системе
    Реализован процесс получения сведений о полномочиях, имеющихся при сканировании.

--- usbddc/src/main/java/ru/pel/usbddc/collector/SystemInfoCollector.java
+++ usbddc/src/main/java/ru/pel/usbddc/collector/SystemInfoCollector.java
@@ -44,8 +44,6 @@ public class SystemInfoCollector {
 
     private List<Callable<Map<String, USBDevice>>> analyzerTaskList = new ArrayList<>();
     private SystemInfo systemInfo;
-    //TODO #52
-    // private String scannedWithAdminPrivileges;
 
     public SystemInfoCollector() {
         systemInfo = new SystemInfo();
@@ -64,28 +62,28 @@ public class SystemInfoCollector {
      */
     public SystemInfoCollector collectSystemInfo() {
         long startTime = System.currentTimeMillis();
-
+        systemInfo.setScannedWithAdminPrivileges(RegistryAnalyzer.determineAdminPermissionsGranted());
         Future<OSInfo> osInfoFuture = executorService.submit(() -> new OSInfoCollector().collectInfo());
         Future<String> uuidFuture = executorService.submit(this::getSystemUUID);
 
-        List<Path> logList = new OSInfoCollector().getSetupapiDevLogList();
-
         addAnalyzer(new RegistryAnalyzer(true));
+
+        List<Path> logList = new OSInfoCollector().getSetupapiDevLogList();
         addAnalyzer(new SetupapiDevLogAnalyzer(logList, true));
         try {
             executeAnalysis();
             systemInfo.setUuid(uuidFuture.get());
-            systemInfo.setOsInfo(osInfoFuture.get(/*30, TimeUnit.SECONDS*/));
+            systemInfo.setOsInfo(osInfoFuture.get(30, TimeUnit.SECONDS));
 
         } catch (InterruptedException | ExecutionException e) {
             LOGGER.error("Текущий поток был прерван во время работы анализатора. Причина: {}", e.getLocalizedMessage());
             LOGGER.debug("Текущий поток был прерван во время работы анализатора.", e);
             Thread.currentThread().interrupt();
-        } /*catch (TimeoutException e) {
+        } catch (TimeoutException e) {
             LOGGER.error("Анализатор работал слишком долго. {}", e.getLocalizedMessage());
             LOGGER.debug("Анализатор работал слишком долго. ", e);
             Thread.currentThread().interrupt();
-        }*/
+        }
 
 //        executorService.shutdown();
         LOGGER.trace("Время работы общее - {}мс", System.currentTimeMillis() - startTime);
