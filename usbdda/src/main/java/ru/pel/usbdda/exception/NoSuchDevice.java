ed0900ec2f609abc9b4090e9e14b988e9809e16c
Quitest <Quitest>
2022-06-19 00:08:19 +0300

    USBDDx-68 UsbDeviceController + HATEOAS
    Переименование NoSuchDevice -> NoSuchDeviceException

--- usbdda/src/main/java/ru/pel/usbdda/exception/NoSuchDevice.java
+++
@@ -1,9 +0,0 @@
-package ru.pel.usbdda.exception;
-
-import java.util.NoSuchElementException;
-
-public class NoSuchDevice extends NoSuchElementException {
-    public NoSuchDevice(String msg) {
-        super(msg);
-    }
-}
